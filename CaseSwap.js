3dab365778531998ab2ea0baf482bb7da1f3df62
Tania <tania.dev77@gmail.com>
2015-05-12 11:04:10 -0700

    add js for case swap challenge 5

---
+++ CaseSwap.js
@@ -0,0 +1,21 @@
+function testString(str){
+
+    str = str.split('');
+    var newString = [];
+
+    str.forEach(function(s){
+        if(s === s.toUpperCase()){
+          s = s.toLowerCase();
+          newString.push(s);
+        } else {
+           s = s.toUpperCase();
+           newString.push(s);
+        }
+    });
+    newString = newString.join('');
+    return newString;
+}
+var string = "Life is 10% what happens to you, and 90% of how you REACT to it";
+var result = testString(string);
+
+console.log('result: ', result);
